3ea33fbfc2fb2e4d35f6adc97f111315222130d2
Calin Novo <calin_novo@hotmail.com>
2026-01-22 20:11:20 +0000

    Bug fix where refresh can cause permanent lock on webpage

--- simulation/explorer.go
+++ simulation/explorer.go
@@ -70,22 +70,29 @@ func runFrontend(wg *sync.WaitGroup, channel chan VehicleInfoDatagram, mapsim *M
 		reader.Wait()
 		reader.Add(1) //Only one reader allowed at once
 		
-		var ackReceived sync.WaitGroup
-		go RunWebReaderLoop(conn, mapsim, &ackReceived, wg)
+		var webReaderChan chan error = make(chan error)
+		defer close(webReaderChan)
+		go RunWebReaderLoop(conn, mapsim, webReaderChan, wg)
 
-		if err := SendMapSetupMessage(conn, mapsim, &ackReceived); err != nil {
+		if err := SendMapSetupMessage(conn, mapsim); err != nil {
 			fmt.Println("Write Error:", err)
 			return
 		}
-		ackReceived.Wait()
+		if err := <-webReaderChan; err != nil {
+			fmt.Println(err)
+			return
+		}
 
 		run := 0
 		for current_run-run > 0 { //Resend all lost packets in order if page connection is lost.
-			if err := SendWebVehicleMessage(conn, vehicle_fetch_history[run], &ackReceived); err != nil {
+			if err := SendWebVehicleMessage(conn, vehicle_fetch_history[run]); err != nil {
 				fmt.Println("Write Error:", err)
 				return
 			}
-			ackReceived.Wait()
+			if err := <-webReaderChan; err != nil {
+				fmt.Println(err)
+				return
+			}
 			run++
 		}
 		if err := SendLoadingDoneMessage(conn); err != nil {
@@ -101,11 +108,14 @@ func runFrontend(wg *sync.WaitGroup, channel chan VehicleInfoDatagram, mapsim *M
 				vehicles_seen -= mapsim.config_parameters.MAX_VEHICLES
 				current_run++
 
-				if err := SendWebVehicleMessage(conn, vehicle_fetch_history[current_run-1], &ackReceived); err != nil {
+				if err := SendWebVehicleMessage(conn, vehicle_fetch_history[current_run-1]); err != nil {
 					fmt.Println("Write Error:", err)
 					return
 				}
-				ackReceived.Wait()
+				if err := <-webReaderChan; err != nil {
+					fmt.Println(err)
+					return
+				}
 			}
 		}
 
@@ -125,16 +135,15 @@ func SendLoadingDoneMessage(conn *websocket.Conn) error {
 	return conn.WriteJSON(Message{Type:"loading_done_message", Data: ""})
 }
 
-func SendWebVehicleMessage(conn *websocket.Conn, data []VehicleInfoDatagram, ack_received *sync.WaitGroup) error{
+func SendWebVehicleMessage(conn *websocket.Conn, data []VehicleInfoDatagram) error{
 	msg := make([]VehicleMessage, len(data))
 	for i:=0;i<len(data);i++ {
 		msg[i] = VehicleMessage{Vehicle_ID: data[i].id, X: data[i].x, Y: data[i].y, Time: data[i].time, Status: data[i].status}
 	}
-	ack_received.Add(1)
 	return conn.WriteJSON(Message{Type:"vehicle_message", Data: msg})
 }
 
-func SendMapSetupMessage(conn *websocket.Conn, mapsim *Map, ack_received *sync.WaitGroup) error {
+func SendMapSetupMessage(conn *websocket.Conn, mapsim *Map) error {
 	msg := MapSetupMessage{Nodes: make([]MapNodeSetupMessage,len(mapsim.nodes)), Lanes: make([]MapLaneSetupMessage, len(mapsim.lanes)), ConfigParameters: *mapsim.config_parameters}
 	for n:=0;n<len(mapsim.nodes);n++ {
 		msg.Nodes[n] = MapNodeSetupMessage{Node_ID: mapsim.nodes[n].id, X: mapsim.nodes[n].pos.x, Y: mapsim.nodes[n].pos.y, AgentType: mapsim.nodes[n].agent.Descriptor()}
@@ -142,14 +151,13 @@ func SendMapSetupMessage(conn *websocket.Conn, mapsim *Map, ack_received *sync.W
 	for l:=0;l<len(mapsim.lanes);l++ {
 		msg.Lanes[l] = MapLaneSetupMessage{Lane_ID: mapsim.lanes[l].id, Start_X: mapsim.lanes[l].start_pos.x, Start_Y: mapsim.lanes[l].start_pos.y, End_X: mapsim.lanes[l].end_pos.x, End_Y: mapsim.lanes[l].end_pos.y}
 	}
-	ack_received.Add(1)
 	return conn.WriteJSON(Message{Type:"map_setup_message", Data: msg})
 }
 
-func RunWebReaderLoop(conn *websocket.Conn, mapsim *Map, ackReceived *sync.WaitGroup, wg *sync.WaitGroup) {
-	blocking := false
+func RunWebReaderLoop(conn *websocket.Conn, mapsim *Map, webReaderChan chan error, wg *sync.WaitGroup) {
+	wg_blocking := false
 	defer func() {
-		if blocking {
+		if wg_blocking {
 			wg.Done()
 			mapsim.config_parameters.SIM_RATE = 1
 		}
@@ -158,27 +166,30 @@ func RunWebReaderLoop(conn *websocket.Conn, mapsim *Map, ackReceived *sync.WaitG
 		msg, err := ReadWebMessage(conn)
 		if err != nil {
 			fmt.Println("Read Error:", err)
+			webReaderChan <- err
 			return
 		}
 		switch msg.Type {
 		case "ACK":
-			ackReceived.Done()
+			webReaderChan <- nil
 		case "sim_rate_update":
 			rate, ok := msg.Data.(float64)
 			if !ok {
 				fmt.Println("Error: SIM_RATE is not a float!")
+				webReaderChan <- fmt.Errorf("Error: SIM_RATE is not a float!")
 				return
 			}
 			if mapsim.config_parameters.SIM_RATE != rate && rate == 0 {
 				wg.Add(1)
-				blocking = true
+				wg_blocking = true
 			} else if mapsim.config_parameters.SIM_RATE != rate && mapsim.config_parameters.SIM_RATE == 0 {
 				wg.Done()
-				blocking = false
+				wg_blocking = false
 			}
 			mapsim.config_parameters.SIM_RATE = rate
 		default:
 			fmt.Println("Read Error: Unknown Web Message!")
+			webReaderChan <- fmt.Errorf("Read Error: Unknown Web Message!")
 			return
 		}
 	}
